5dbca1d07378682ffb1434286707a66208d1af01
Joshua K. Farrar <joshua@sent1nel.me>
2014-12-29 17:11:10 -0500

    wait to attach event listeners until game is loaded

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,13 @@
 define(['jquery', 'game'], function($, Game) {
   var game;
 
-  $(document).ready(function() {
+  var initApp = function() {
+    $(document).ready(function() {
+      initGame();
+    });
+  };
+
+  var initGame = function() {
     require(['game'], function(Game) {
       var canvas = $('canvas')[0];
 
@@ -11,35 +17,37 @@ define(['jquery', 'game'], function($, Game) {
       if (!game.started) {
         game.run();
       }
+
+      $('body').unbind('click');
+      $('body').click(function(event) {
+        if (game.started) {
+          game.click();
+        }
+      });
+
+      $(document).mousemove(function(event) {
+        var gamePos = $('canvas').offset(),
+            width = game.renderer.getWidth(),
+            height = game.renderer.getHeight(),
+            mouse = game.mouse;
+
+        mouse.x = event.pageX - gamePos.left;
+        mouse.y = event.pageY - gamePos.top;
+
+        if(mouse.x <= 0) {
+            mouse.x = 0;
+        } else if(mouse.x >= width) {
+            mouse.x = width - 1;
+        }
+
+        if(mouse.y <= 0) {
+            mouse.y = 0;
+        } else if(mouse.y >= height) {
+            mouse.y = height - 1;
+        }
+      });
     });
-  });
+  };
 
-  $('body').unbind('click');
-  $('body').click(function(event) {
-    if (game.started) {
-      game.click();
-    }
-  });
-
-  $(document).mousemove(function(event) {
-    var gamePos = $('canvas').offset(),
-        width = game.renderer.getWidth(),
-        height = game.renderer.getHeight(),
-        mouse = game.mouse;
-
-    mouse.x = event.pageX - gamePos.left;
-    mouse.y = event.pageY - gamePos.top;
-
-    if(mouse.x <= 0) {
-        mouse.x = 0;
-    } else if(mouse.x >= width) {
-        mouse.x = width - 1;
-    }
-
-    if(mouse.y <= 0) {
-        mouse.y = 0;
-    } else if(mouse.y >= height) {
-        mouse.y = height - 1;
-    }
-  });
+  initApp();
 });

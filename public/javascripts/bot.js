40569f040307d0dfca8f24efc069cb5b0e5e9dd7
Joshua K. Farrar <joshua@sent1nel.me>
2015-01-02 03:43:55 +0000

    reorganize bot method

--- public/javascripts/bot.js
+++ public/javascripts/bot.js
@@ -1,6 +1,7 @@
 define(['player', 'marker', 'board', 'solver', 'o', 'x'], function(Player, Marker, Board, Solver, O, X) {
   var Bot = Player.extend({
     init: function(name, marker, board) {
+      this.INITIAL_DEPTH = 0;
       this.name = name;
       this.marker = marker;
       this.board = board;
@@ -9,7 +10,7 @@ define(['player', 'marker', 'board', 'solver', 'o', 'x'], function(Player, Marke
       this.nextMove;
     },
     
-    move: function () {
+    move: function() {
       if (this.thinking) {
         return false;
       }
@@ -23,24 +24,45 @@ define(['player', 'marker', 'board', 'solver', 'o', 'x'], function(Player, Marke
     },
     
     calculateBestMove: function() {
-      var depth = 0;
-      this.minimax(new Solver(this.board), depth);
+      this.minimax(new Solver(this.board), this.INITIAL_DEPTH);
     },
-    
-    // this is a board clone method because of issues with
-    // _.clone not deeply cloning child objects and arrays
-    // instead, simply just referencing them
-    cloneBoard: function(board) {
-      var childBoard = new Board();
-      for (var x = 0; x < 3; x++) {
-        for (var y = 0; y < 3; y++) {
-          var marker = board.getMarkerAt(x, y);
-          if (marker) {
-            childBoard.addMarker(marker);
-          }
-        }
+
+    minimax: function(solver, depth) {
+      if (solver.hasWinner()) {
+        var score = this.scoreGame(solver, depth);
+        return score;
       }
-      return childBoard;
+      
+      var board = solver.board,
+          possibleMoves = board.getAvailableMoves();
+          
+      if (!possibleMoves) {
+        throw "No possible moves!";
+      }
+
+      var candidates = [],
+          nextMarker = this.nextMarker(depth),
+          self = this;
+
+      for (var i = 0; i < possibleMoves.length; i++) {
+        var move = possibleMoves[i],
+            nextMarker = this.nextMarker(depth),
+            childBoard = this.cloneBoard(board);
+
+        childBoard.addMarker(new nextMarker(move.x, move.y));
+
+        var childSolver = new Solver(childBoard);
+        childSolver.checkVictoryConditions();
+        
+        var score = this.minimax(childSolver, depth + 1);
+        
+        candidates.push({ score: score, move: move });
+      }
+      
+      var bestCandidate = (this.isMyTurn(depth)) ? this.maximize(candidates) : this.minimize(candidates);
+      this.nextMove = bestCandidate.move;
+      
+      return bestCandidate.score || 0;
     },
     
     scoreGame: function(solver, depth) {
@@ -61,7 +83,7 @@ define(['player', 'marker', 'board', 'solver', 'o', 'x'], function(Player, Marke
         return marker;
       }
     },
-    
+
     isMyTurn: function(depth) {
       if (depth % 2) {
         return false;
@@ -70,44 +92,28 @@ define(['player', 'marker', 'board', 'solver', 'o', 'x'], function(Player, Marke
       }
     },
     
-    minimax: function(solver, depth) {
-      if (solver.hasWinner()) {
-        var score = this.scoreGame(solver, depth);
-        return score;
+    // this is a board clone method because of issues with
+    // _.clone not deeply cloning child objects and arrays
+    // instead, simply just referencing them
+    cloneBoard: function(board) {
+      var childBoard = new Board();
+      for (var x = 0; x < 3; x++) {
+        for (var y = 0; y < 3; y++) {
+          var marker = board.getMarkerAt(x, y);
+          if (marker) {
+            childBoard.addMarker(marker);
+          }
+        }
       }
-      
-      var candidates = [],
-          board = solver.board,
-          possibleMoves = board.getAvailableMoves();
-          
-      if (!possibleMoves) {
-        throw "No possible moves!";
-      }
-
-      for (var i = 0; i < possibleMoves.length; i++) {
-        var move = possibleMoves[i],
-            nextMarker = this.nextMarker(depth),
-            childBoard = this.cloneBoard(board);
-
-        childBoard.addMarker(new nextMarker(move.x, move.y));
-
-        var childSolver = new Solver(childBoard);
-        childSolver.checkVictoryConditions();
-        
-        var score = this.minimax(childSolver, depth + 1);
-        
-        candidates.push({ score: score, move: move });
-      }
-      
-      var bestCandidate;
-      if (this.isMyTurn(depth)) {
-        bestCandidate = _.max(candidates, function(candidate) { return candidate.score; });
-      } else {
-        bestCandidate = _.min(candidates, function(candidate) { return candidate.score; });
-      }
-
-      this.nextMove = bestCandidate.move;
-      return bestCandidate.score || 0;
+      return childBoard;
+    },
+    
+    maximize: function(candidates) {
+      return _.max(candidates, function(candidate) { return candidate.score; });
+    },
+    
+    minimize: function(candidates) {
+      return _.min(candidates, function(candidate) { return candidate.score; });
     }
   });
   
